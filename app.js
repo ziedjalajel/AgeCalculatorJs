3515906a950afbd0bab7267b0a4cd4854701dde8
ziedjalajel <zied.jalajel1993@gmail.com>
2021-05-19 19:56:39 +0300

    Finally i'm done

--- app.js
+++ app.js
@@ -14,17 +14,50 @@
 // This functions should check the integrity of the parameters and pass true/false
 function checkParamsFn(year, month, day) {
   // Write your code here
+  if(typeof year === 'number' && typeof month === 'number' && typeof day === 'number') {
+    return true}
+ else {return false}
+ 
 }
 
 // This functions checks if the person is or above 18 years of age, return true/false
 function checkOverEighteenFn(year, month, day) {
   // Write your code here
-}
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0');
+  let yyyy = today.getFullYear();
+  if(parseInt(mm)>month){
+    return ((parseInt(yyyy) - year)>=18)
+  }
+  else if(parseInt(dd)>=day&&parseInt(mm)===month){
+    return ((parseInt(yyyy) - year)>=18)
+  }
+  return ((parseInt(yyyy) - year -1)>=18)
+  }
 
 function calculateAgeFn(year, month, day) {
   // Write your code here
+  if(checkParamsFn(year,month,day)&&checkOverEighteenFn(year,month,day)){
+    let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0');
+  let yyyy = today.getFullYear();
+  if(parseInt(mm)>month){
+    return (parseInt(yyyy) - year)
+  }
+  else if(parseInt(dd)>=day&&parseInt(mm)===month){
+    return (parseInt(yyyy) - year)
+  }
+  return (parseInt(yyyy) - year -1)
+  }
+  
+  else{
+    return "error"
+  }
+ 
 }
-
+console.log(calculateAgeFn(2003,5,19))
 // Look at the naming of the functions. it looks like salwaBaqer, where
 // the first letter of the first word is small, while the first letter of the
 // second word is Capital. This is called "Camel Casing."
